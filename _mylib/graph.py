9b1ccc34b6c0dfcc37b038a457e91913e4fc222d
pg-endokenta <k.endo@4nonome.com>
2024-02-15 23:06:10 +0900

    2024/02/15/23:06

---
+++ _mylib/graph.py
@@ -0,0 +1,27 @@
+# グラフの頂点1と繋がっている最大頂点
+from collections import defaultdict, deque
+
+n: int
+n = int(input())
+
+graph = defaultdict(list)
+a: int
+b: int
+for i in range(n):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+seen: set[int] = set()
+seen.add(1)
+queue: deque[int] = deque()
+queue.append(1)
+
+while queue:
+    v = queue.popleft()
+    for next_v in graph[v]:
+        if next_v not in seen:
+            seen.add(next_v)
+            queue.append(next_v)
+
+print(max(seen))
